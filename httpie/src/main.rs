b5fb1e9ad3fc54bdc61930ad1dd7b607d7bad427
geyuanjun <geyuanjun@bilibili.com>
2023-07-21 01:19:04 +0800

    feat: add httpie first feature

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,3 +1,33 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(version = "1.0", author = "geyuanjun daydaygyj@gmail.com")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+// 子命令分别对应不同的 HTTP 方法，目前只支持 get / post
+#[derive(Parser, Debug)]
+enum SubCommand {
+    Get(Get),
+    Post(Post),
+}
+
+// get function
+#[derive(Parser, Debug)]
+struct Get {
+    url: String,
+}
+
+// post function
+#[derive(Parser, Debug)]
+struct Post {
+    url: String,
+    body: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    println!("{:?}", opts)
 }
